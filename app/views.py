30032c99ea92258c42ee7551bce1148499a74a15
kelg06 <mgladney@basecampcodingacademy.org>
2024-10-16 16:06:54 -0500

    comme

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.http.response import HttpResponse
+from django.http.request import HttpRequest
+
+
+def hello_view(request: HttpRequest) ->HttpResponse:
+    return HttpResponse("Hello World")
---
+++ config/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path
+
+from app.views import hello_view
+
+
+urlpatterns = [
+    path("hello/", hello_view),
+    path("admin/", admin.site.urls),
+]
